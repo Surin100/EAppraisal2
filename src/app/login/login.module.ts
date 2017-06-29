ed9cdbea73c6bd733509ab91b375948255931215
nhatsovico <nhatsovico@gmail.com>
2017-06-29 09:16:59 +0700

    Routes Init

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login.component';
+import { loginRoutes } from './login.routes';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(loginRoutes)
   ],
   declarations: [LoginComponent]
 })
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule} from '@angular/router';
+
 import { MainComponent } from './main.component';
+import { mainRoutes} from './main.routes';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(mainRoutes)
   ],
   declarations: [MainComponent]
 })
