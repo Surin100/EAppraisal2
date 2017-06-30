0f8fd877f129bbadd31c0716bb3db56f7a5c847e
nhatsovico <nhatsovico@gmail.com>
2017-06-30 14:48:54 +0700

    main menu

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
 
 import {AuthenService} from '../core/services/authen.service';
 import {LoggedInUser} from '../core/domain/loggedin.user';
 import {SystemConstants} from '../core/common/system.constants';
+import {UrlConstants} from '../core/common/url.constants';
 
 @Component({
   selector: 'app-main',
@@ -11,7 +13,7 @@ import {SystemConstants} from '../core/common/system.constants';
 })
 export class MainComponent implements OnInit {
   user: LoggedInUser;
-  constructor(private _authenService:AuthenService) { }
+  constructor(private _authenService:AuthenService, private _router:Router) { }
 
   ngOnInit() {
     this.user = JSON.parse(localStorage.getItem(SystemConstants.CURRENT_USER));
@@ -19,6 +21,30 @@ export class MainComponent implements OnInit {
   }
   logout(){
     this._authenService.logout();
+  }
+
+  homeClick(){
+    this._router.navigate([UrlConstants.HOME]);
+  }
+
+  appraisalClick(){
+    
+  }
+
+  goalClick(){
+
+  }
+
+  guidelineClick(){
+
+  }
+
+  reportClick(){
+
+  }
+
+  registerClick(){
+    this._router.navigate([UrlConstants.REGISTER]);
   }
 
 }
