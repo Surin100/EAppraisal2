07275c5fcbbb499d3f564361ba2e70f03dc44ead
nhatsovico <nnhatsovico@gmail.com>
2017-07-20 00:22:25 +0700

    ResetPassword

--- src/app/main/register/register.component.ts
+++ src/app/main/register/register.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DataService } from '../../core/services/data.service';
+import { NotificationService } from '../../core/services/notification.service';
+import { HandleErrorService } from '../../core/services/handle-error.service';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  user:any ={};
+  resetPasswordLoading:Boolean =false;
+  constructor(private _dataService: DataService, private _notificationService:NotificationService,
+    private _handleErrorService: HandleErrorService
+  ) { }
 
   ngOnInit() {
+  }
+
+  resetPassword(){
+    if(!this.user.userName) return;
+    this.resetPasswordLoading = true;
+    // debugger
+    this._dataService.post('/api/Account/ResetPassword',this.user).subscribe((response:any)=>{
+      this._notificationService.printSuccessMessage('The password has been reset.');
+      this.resetPasswordLoading = false;
+    }, error=> {
+      this._handleErrorService.handleError(error);
+      this.resetPasswordLoading = false;
+    });
   }
 
 }
--- src/app/main/register/register.module.ts
+++ src/app/main/register/register.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RegisterComponent } from './register.component';
+import { FormsModule } from '@angular/forms';
 import {RouterModule} from '@angular/router';
+
+import { RegisterComponent } from './register.component';
 import {registerRoutes} from './register.routes';
+import { DataService } from '../../core/services/data.service';
+import { NotificationService } from '../../core/services/notification.service';
+import { HandleErrorService } from '../../core/services/handle-error.service';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(registerRoutes)
   ],
+  providers:[DataService, NotificationService, HandleErrorService],
   declarations: [RegisterComponent]
 })
 export class RegisterModule { }
