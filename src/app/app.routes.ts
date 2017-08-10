eb57fae37cdb2f3918c8bf2732bf95d316dba07d
nhatsovico <nnhatsovico@gmail.com>
2017-08-10 14:45:55 +0700

    mainjs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 
-import {AuthenGuard} from './core/guard/authen.guard';
-import {NewUserGuard} from './core/guard/new-user.guard';
-import {NonAuthenGuard} from './core/guard/non-authen.guard';
+import { AuthenGuard } from './core/guard/authen.guard';
+import { NewUserGuard } from './core/guard/new-user.guard';
+import { NonAuthenGuard } from './core/guard/non-authen.guard';
 
 export const appRoutes: Routes = [
     { path: '', redirectTo: 'main', pathMatch: 'full' },
-    { path: 'login', loadChildren: './login/login.module#LoginModule', canActivate:[NonAuthenGuard]},
-    { path: 'main', loadChildren: './main/main.module#MainModule',canActivate:[NewUserGuard] },
-    { path: 'ChangePassword', loadChildren:'./change-password/change-password.module#ChangePasswordModule',canActivate:[AuthenGuard]}
+    { path: 'main', loadChildren: './main/main.module#MainModule', canActivate: [NewUserGuard] },
+    { path: 'login', loadChildren: './login/login.module#LoginModule', canActivate: [NonAuthenGuard] },
+    { path: 'ChangePassword', loadChildren: './change-password/change-password.module#ChangePasswordModule', canActivate: [AuthenGuard] }
 ]
